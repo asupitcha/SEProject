8d6b895eef4847b0d872f2cd167eda8850e6b67f
Doonlap <33442621+Doonlap@users.noreply.github.com>
2017-11-07 22:23:00 +0700

    Add files via upload

---
+++ SEProject/src/FillScoreController.java
@@ -0,0 +1,97 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FillScoreController {
+	private String[][] data;
+	private String fileName;
+	private int row,column;
+	private int gradeGrilienia;
+	private String[] g;
+	
+	public FillScoreController(String fileName) {		
+		BufferedReader bf;
+		String line = "";
+		String[] str;
+		this.fileName = fileName;
+		
+		try {
+			bf = new BufferedReader(new FileReader(new File(fileName)));
+			while((line = bf.readLine()) != null){
+				str = line.split(" ");
+				column = str.length;
+				row++;
+			}
+			bf.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public int getRow(){
+		return row;
+	}
+	public String[] readFileGradeGrilienia(String fileName){
+		BufferedReader bf;
+		String line = "";
+		int i=0;
+		try {
+			bf = new BufferedReader(new FileReader(new File(fileName)));
+			line = bf.readLine();
+			gradeGrilienia = Integer.parseInt(line);
+			g = new String[gradeGrilienia];
+			while((line = bf.readLine()) != null){
+				g[i] = line;
+				i++;
+			}
+			bf.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+		return g;
+	}
+	public void readFile(){
+		data = new String[row][column+gradeGrilienia];
+		BufferedReader bf;
+		String line = "";
+		String[] str;
+		int row = 0;
+
+		try {
+			bf = new BufferedReader(new FileReader(new File(fileName)));
+			while((line = bf.readLine()) != null){
+				str = line.split(" ");
+				for(int i=0; i<str.length; i++){
+					data[row][i] = str[i];
+				} row++;
+			}
+			bf.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public String[][] getData(){
+		return data;
+	}
+	public void saveScoreFile(String filename) throws FileNotFoundException{
+		PrintWriter out = new PrintWriter(filename);
+		for(int i=0; i<row; i++){
+			for(int j=0; j<column+gradeGrilienia; j++){
+				out.print(data[i][j] + " ");
+			}
+			out.println();
+		}
+		out.close();
+	}
+	public void addScore(String score, int x, int y){
+		data[x][y] = score;
+	}
+}
